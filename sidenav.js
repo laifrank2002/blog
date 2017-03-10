806f97109d259e6aceabe33f826ffd0505b9c3d4
laifrank2002 <franklai2002@gmail.com>
2017-03-09 19:18:36 -0500

    Add files via upload

---
+++ sidenav.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("mySidenav").style.display = "block";
+	document.getElementById("rightarrow").style.display = "none";
+	document.getElementById("leftarrow").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.display = "none";
+	document.getElementById("rightarrow").style.display = "block";
+	document.getElementById("leftarrow").style.display = "none";
+}
